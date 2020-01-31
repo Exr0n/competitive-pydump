da185cab34acbd1a951ce04502b0edea39f13573
Exr0n <spotyie@gmail.com>
2020-01-30 19:49:02 -0800

    PLM accounts-merge.py

--- leetcode/accounts-merge.py
+++ leetcode/accounts-merge.py
@@ -1,32 +1,52 @@
-# accounts = [["John", "johnsmith@mail.com", "john00@mail.com"], ["John", "johnnybravo@mail.com"], ["John", "johnsmith@mail.com", "john_newyork@mail.com"], ["Mary", "mary@mail.com"]]
+accounts = [["John", "johnsmith@mail.com", "john00@mail.com"], ["John", "johnnybravo@mail.com"], ["John", "john_newyork@mail.com", "johnsmith@mail.com", "johnsmith_newyork@mail.com"], ["Mary", "mary@mail.com"]]
 
-
-class Account:
-  def __init__(self, name, *emails):
-    self.name = name
-    self.emails = emails
-
-
+emails = {}
 class DSNode:
-  pass
+    def __init__(self, email: str, father: str, name: str):
+        self.email = email
+        self.father = father
+        self.name = name
 
-with open("prob.in", "r") as rf:
-  # for line in open("prob.in")
-  accounts = list(map(lambda line: Account(*line.split())))
-  accounts = [Account(*line.split()) for line in rf]
+    @property
+    def father(self):
+        if self.email != self.__father:
+            self.__father = emails[self.__father].father
+        return self.__father
 
-print(map(lambda x: x*2, range(10)))
+    @father.setter
+    def father(self, name: str):
+        self.__father = name
+        
+    def __repr__(self):
+        return 'DSNode({}, {}->{})'.format(self.name, self.email, self.father)
 
 
-'''
-2d grid 
-some coords have obstacles
+# class Account:
+#     def __init__(self, name, *emails):
+#         self.name = name
+#         self.emails = emails
+#
+# with open("prob.in", "r") as rf:
+#     # for line in open("prob.in")
+#     accounts = list(map(lambda line: Account(*line.split())))
+#     accounts = [Account(*line.split()) for line in rf]
+#     for acct in accounts:
+#         for email in emails:
+#             emails[email] = DSNode(email, acct.emails[0], acct.name)
 
-N = 5000
+for acct in accounts:
+    for email in acct[1:]:
+        if email in emails:
+            emails[acct[1]].father = emails[email].father
+        else:
+            emails[email] = DSNode(email, acct[1], acct[0])
 
-16 * 25M^2
+out = {}
+for email in emails:
+    acct_email = emails[email].father
+    if not acct_email in out:
+        out[acct_email] = [emails[email].name]
+    out[acct_email].append(email)
 
-2 robots, go to exit
-commands: forward, right, left
-
-'''
+ret = [[acct[0]] + sorted(acct[1:]) for acct in out.values()]
+print(ret)
